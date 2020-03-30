3947c800c7ccada98543282a3cdda355e49c7d13
ankur <dec22ankur@gmail.com>
2020-03-30 12:01:07 +0530

    saving to github

---
+++ build/precache-manifest.e9546a93ead0d472dfabadde20b9fbee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb38690786e9d84842ce",
+    "url": "/simple-information-system/static/css/main.3ef0dee8.chunk.css"
+  },
+  {
+    "revision": "fb38690786e9d84842ce",
+    "url": "/simple-information-system/static/js/main.fb386907.chunk.js"
+  },
+  {
+    "revision": "5ef3b90050b60f0bfacb",
+    "url": "/simple-information-system/static/js/1.5ef3b900.chunk.js"
+  },
+  {
+    "revision": "c5b88660c802a1cad0ab",
+    "url": "/simple-information-system/static/js/runtime~main.c5b88660.js"
+  },
+  {
+    "revision": "f833a5cf8e9d348546c0f077070a2d3c",
+    "url": "/simple-information-system/index.html"
+  }
+];
